974ef5c75468042dcb5993d4fee295e335c68763
Chris Titus <dfm.titus@gmail.com>
2021-11-19 11:47:30 -0600

    cleanup and theme setup

--- dotfiles/rofi/powermenu/powermenu.sh
+++ dotfiles/rofi/powermenu/powermenu.sh
@@ -15,7 +15,7 @@
 # full_circle     full_square     full_rounded     full_alt
 # row_circle      row_square      row_rounded      row_alt
 
-theme="drop_circle"
+theme="drop_square"
 dir="$HOME/.config/rofi/powermenu"
 
 
@@ -50,24 +50,10 @@ options="$shutdown\n$reboot\n$lock\n$suspend\n$logout"
 chosen="$(echo -e "$options" | $rofi_command -p "Uptime: $uptime" -dmenu -selected-row 2)"
 case $chosen in
     $shutdown)
-		ans=$(confirm_exit &)
-		if [[ $ans == "yes" || $ans == "YES" || $ans == "y" || $ans == "Y" ]]; then
 			systemctl poweroff
-		elif [[ $ans == "no" || $ans == "NO" || $ans == "n" || $ans == "N" ]]; then
-			exit 0
-        else
-			msg
-        fi
         ;;
     $reboot)
-		ans=$(confirm_exit &)
-		if [[ $ans == "yes" || $ans == "YES" || $ans == "y" || $ans == "Y" ]]; then
 			systemctl reboot
-		elif [[ $ans == "no" || $ans == "NO" || $ans == "n" || $ans == "N" ]]; then
-			exit 0
-        else
-			msg
-        fi
         ;;
     $lock)
 		if [[ -f /usr/bin/i3lock ]]; then
