dd830adcf3e1d303ed3ff8cbe45204bff0ce9fb9
Chris Titus <dfm.titus@gmail.com>
2021-11-30 14:29:01 -0600

    Update root.sh

--- root.sh
+++ root.sh
@@ -5,11 +5,3 @@ apt install bspwm polybar sddm rofi kitty thunar flameshot neofetch sxhkd git lx
 # Download Nordic Theme
 cd /usr/share/themes/
 git clone https://github.com/EliverLara/Nordic.git
-mkdir -p /usr/share/sddm/themes/Nordic
-cp Nordic/kde/sddm/* /usr/share/sddm/themes/Nordic/
-
-# Make SDDM Config
-cat <<EOF > /etc/sddm.conf
-[Theme]
-Current=Nordic
-EOF
