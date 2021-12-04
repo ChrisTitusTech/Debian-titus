297b8a9abb99811370dac74d5eed96d6879c43d2
fluffycloud <68042667+fluffycloud331@users.noreply.github.com>
2021-12-04 10:29:06 -0500

    Update user.sh
    add Ms fonts to installer

--- user.sh
+++ user.sh
@@ -13,6 +13,9 @@ wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Meslo.zip
 unzip Meslo.zip -d ~/.fonts   
 fc-cache -vf
 
+#Ms-fonts
+sudo apt install ttf-mscorefonts-installer
+
 # Layan Cursors
 cd "$HOME/build"
 git clone https://github.com/vinceliuice/Layan-cursors
