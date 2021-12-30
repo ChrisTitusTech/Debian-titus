c3e0e51e4d5bea5d67d12e6a88c8b0afc0c65b65
Chris Titus <dfm.titus@gmail.com>
2021-12-30 14:14:31 -0600

    bug fixes
    -Background addition
    -Fix various bspwm issues
    -Fix user setup to put files in $HOME folder

--- user.sh
+++ user.sh
@@ -4,20 +4,20 @@
 sudo apt install papirus-icon-theme lxappearance fonts-noto-color-emoji fonts-firacode fonts-font-awesome libqt5svg5 qml-module-qtquick-controls
 
 # Make Theme folders
-mkdir -p ~/.themes ~/.fonts ~/.config
+mkdir -p $HOME/.themes $HOME/.fonts $HOME/.config
 
 # Fira Code Nerd Font variant needed
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip
-unzip FiraCode.zip -d ~/.fonts
+unzip FiraCode.zip -d $HOME/.fonts
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Meslo.zip
-unzip Meslo.zip -d ~/.fonts   
+unzip Meslo.zip -d $HOME/.fonts   
 fc-cache -vf
 
 #Ms-fonts
 sudo apt install ttf-mscorefonts-installer
 
 # Layan Cursors
-mkdir -p ~/build
+mkdir -p $HOME/build
 cd "$HOME/build"
 git clone https://github.com/vinceliuice/Layan-cursors
 cd Layan-cursors
@@ -25,6 +25,6 @@ sudo ./install.sh
 
 echo "RUN LXAPPEARANCE"
 cd ../
-cp .Xresources ~
-cp .Xnord ~
-cp -R dotfiles/* ~/.config/
+cp .Xresources $HOME
+cp .Xnord $HOME
+cp -R dotfiles/* $HOME/.config/
