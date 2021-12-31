592faea2897a3235441ef4c8a22ff855e49f4a90
Chris Titus <dfm.titus@gmail.com>
2021-12-31 11:28:47 -0600

    single script run

--- root.sh
+++ root.sh
@@ -4,6 +4,9 @@
 cp /etc/apt/sources.list /etc/apt/sources.list.bak
 cp sources.list /etc/apt/sources.list 
 
+
+username=$(id -u -n 1000)
+
 # Add Custom Titus Rofi Deb Package
 dpkg -i 'Custom Packages/rofi_1.7.0-1_amd64.deb'
 
@@ -11,8 +14,23 @@ dpkg -i 'Custom Packages/rofi_1.7.0-1_amd64.deb'
 apt update
 
 # Add base packages
-apt install unzip picom bspwm polybar sddm rofi kitty thunar flameshot neofetch sxhkd git lxpolkit lxappearance xorg
+apt install unzip picom bspwm polybar sddm rofi kitty thunar flameshot neofetch sxhkd git lxpolkit lxappearance xorg -y
+apt install papirus-icon-theme lxappearance fonts-noto-color-emoji fonts-firacode fonts-font-awesome libqt5svg5 qml-module-qtquick-controls -y
 
 # Download Nordic Theme
 cd /usr/share/themes/
 git clone https://github.com/EliverLara/Nordic.git
+
+# Fira Code Nerd Font variant needed
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v1.1.0/FiraCode.zip
+unzip FiraCode.zip -d /usr/share/fonts
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v1.1.0/Meslo.zip
+unzip Meslo.zip -d /usr/share/fonts
+fc-cache -vf
+
+
+cp .Xresources /home/$username
+cp .Xnord /home/$username
+cp -R dotfiles/* /home/$username/.config/
+chown -R $username:$username /home/$username
+
--- user.sh
+++ user.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 
 # Appearance pacakges
-sudo apt install papirus-icon-theme lxappearance fonts-noto-color-emoji fonts-firacode fonts-font-awesome libqt5svg5 qml-module-qtquick-controls
-
-# Make Theme folders
-mkdir -p $HOME/.themes $HOME/.fonts $HOME/.config
-
-# Fira Code Nerd Font variant needed
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip
-unzip FiraCode.zip -d $HOME/.fonts
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Meslo.zip
-unzip Meslo.zip -d $HOME/.fonts   
-fc-cache -vf
 
 #Ms-fonts
 sudo apt install ttf-mscorefonts-installer
@@ -22,9 +11,3 @@ cd "$HOME/build"
 git clone https://github.com/vinceliuice/Layan-cursors
 cd Layan-cursors
 sudo ./install.sh
-
-echo "RUN LXAPPEARANCE"
-cd ../
-cp .Xresources $HOME
-cp .Xnord $HOME
-cp -R dotfiles/* $HOME/.config/
