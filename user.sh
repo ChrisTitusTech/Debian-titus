1c8bcfca26f3ba5a4f5b47cb2e0a029b0b834952
nxjosephofficial <nxjosephofficial@protonmail.com>
2022-09-10 21:00:00 +0300

    powermenu unicode fix

--- user.sh
+++ user.sh
@@ -11,6 +11,11 @@ cd "$HOME/build"
 git clone https://github.com/vinceliuice/Layan-cursors
 cd Layan-cursors
 sudo ./install.sh
+cd ../
 sudo mkdir -p /usr/share/icons/default/
 echo "[Icon Theme]" | sudo tee /usr/share/icons/default/index.theme > /dev/null
 echo "Inherits=Layan-cursors" | sudo tee -a /usr/share/icons/default/index.theme > /dev/null
+# Powermenu Unicode problem fix
+git clone https://github.com/adi1090x/rofi.git
+mkdir -p $HOME/.local/share/fonts
+cp -rf rofi/fonts/* $HOME/.local/share/fonts/
