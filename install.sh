b1438822c14d7d07c74ae675f19c2a535ccf0a6e
djoma98 <111081938+djoma98@users.noreply.github.com>
2022-10-13 16:30:40 +0200

    remove sddm

--- install.sh
+++ install.sh
@@ -37,7 +37,7 @@ tar -xzvf sugar-candy.tar.gz -C /usr/share/sddm/themes
 mv /home/$username/.config/sddm.conf /etc/sddm.conf
 
 # Installing Essential Programs 
-apt install sddm bspwm sxhkd kitty rofi polybar picom thunar nitrogen lxpolkit x11-xserver-utils unzip yad pulseaudio pavucontrol -y
+apt install bspwm sxhkd kitty rofi polybar picom thunar nitrogen lxpolkit x11-xserver-utils unzip yad pulseaudio pavucontrol -y
 # Installing Other less important Programs
 apt install psmisc mangohud vim lxappearance papirus-icon-theme lxappearance fonts-noto-color-emoji -y
 #Installing sugar-candy dependencies
