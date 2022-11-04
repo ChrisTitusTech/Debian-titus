c6cf4d1bc40e69b74002b3ce29337800136f5d63
Windows 20 <80408876+win20-official@users.noreply.github.com>
2022-11-04 22:49:57 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -48,7 +48,7 @@ git clone https://github.com/EliverLara/Nordic.git
 
 # Installing fonts
 cd $builddir
-apt install fonts-font-awesome
+nala install fonts-font-awesome
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip
 unzip FiraCode.zip -d /home/$username/.fonts
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Meslo.zip
@@ -69,11 +69,11 @@ cd $builddir
 rm -rf Nordzy-cursors
 
 # Install brave-browser
-sudo apt install apt-transport-https curl -y
+sudo nala install apt-transport-https curl -y
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-sudo apt update
-sudo apt install brave-browser -y
+sudo nala update
+sudo nala install brave-browser -y
 
 # Enable graphical login and change target from CLI to GUI
 systemctl enable sddm
