354446565458addce2818d3a2cdd9b5a81cc0c14
Windows 20 <80408876+win20-official@users.noreply.github.com>
2023-01-03 21:07:45 +0100

    Add spacing fix

--- install.sh
+++ install.sh
@@ -71,7 +71,7 @@ rm -rf Nordzy-cursors
 # Install brave-browser
 sudo nala install apt-transport-https curl -y
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 sudo nala update
 sudo nala install brave-browser -y
 
