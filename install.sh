9532754d8a6bb186c293f58c502c207f40a4b8d3
John Grosshans <JohnGrosshans@GMail.com>
2023-06-19 15:06:56 -0400

    add bat to install.sh
    Adding bat package to install script since beautiful bash has an alias to replace cat with batcat essentially breaking cat if not installed.

--- install.sh
+++ install.sh
@@ -29,7 +29,7 @@ chown -R $username:$username /home/$username
 # Installing Essential Programs 
 nala install feh kitty rofi picom thunar nitrogen lxpolkit x11-xserver-utils unzip wget pulseaudio pavucontrol build-essential libx11-dev libxft-dev libxinerama-dev -y
 # Installing Other less important Programs
-nala install neofetch flameshot psmisc mangohud vim lxappearance papirus-icon-theme lxappearance fonts-noto-color-emoji lightdm -y
+nala install neofetch flameshot psmisc mangohud vim lxappearance papirus-icon-theme lxappearance fonts-noto-color-emoji lightdm bat -y
 
 # Download Nordic Theme
 cd /usr/share/themes/
